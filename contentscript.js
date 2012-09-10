71f7cebf7145a4423f416055ba78226a0e0a8f4d
Nicole Bieber <nbieber@alum.mit.edu>
2012-09-10 11:54:28 -0300

    Update contentscript.js
    cleanup

--- contentscript.js
+++ contentscript.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function($) {
 	    	   mckayla.height=$(this).height()*height_ratio
 	    	   mckayla.style.left=($(this).offset().left+$(this).outerWidth() - orig_width*mckayla.height/orig_height)+"px"
 	    	   mckayla.style.top=($(this).offset().top+$(this).outerHeight() - mckayla.height)+"px"
-	       }
+	   	   }
 	       else
 	       {
 	       	   mckayla.width=$(this).width()*width_ratio
@@ -35,25 +35,3 @@ jQuery(document).ready(function($) {
     	}
     })
 });
-
-// $("img").each(function() {
-//         var randomnumber=Math.floor(Math.random()*10);
-//     	if(randomnumber<=3 &&
-//     	   $(this).attr('src')!=chrome.extension.getURL("mckayla.png") &&
-//     	   $(this)!=undefined &&
-//     	   $(this).attr('src')!=undefined &&
-//     	   $(this).height() >= 50 &&
-//     	   $(this).is(":visible")) {
-    	   	
-//  		   console.log($(this).attr('src'))
-//     	   var mckayla=document.createElement("img")
-//     	   mckayla.src=chrome.extension.getURL("mckayla.png")
-//     	   mckayla.style.position="absolute"
-//     	   mckayla.height=$(this).height()*height_ratio
-//     	   mckayla.style.left=($(this).offset().left+$(this).outerWidth() - 1024*mckayla.height/1483)+"px"
-//     	   mckayla.style.top=($(this).offset().top+$(this).outerHeight() - mckayla.height)+"px"
-//     	   mckayla.style.zIndex=$(this).css("z-index")+1;
-//     	   document.body.appendChild(mckayla)
-//     	}
-// });
-
